b34b4ce6eca52da4f4fd59e9853ef149858d33bb
Eli Chou <eli.r.chou@gmail.com>
2026-08-18 13:57:48 -0500

    Complete foundations exercises 6-11

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...items) {
+    for(item of items){
+        while(arr.includes(item)){
+            arr.splice(arr.indexOf(item),1);
+        }
+    }
+    return arr;
+
 };
 
 // Do not edit below this line
